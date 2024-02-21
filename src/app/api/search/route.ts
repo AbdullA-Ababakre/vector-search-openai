a5e195662d75c9b930d348ecafca612782ab5046
Abdulla-Ababakre <abdullaszu@gmail.com>
2024-02-20 20:43:06 -0800

    fix: fixed the search null problem

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -2,11 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 import OpenAI from "openai";
 import { supabaseClient } from "../../../../utils/supabaseClient";
-// Initialize the Supabase client with your Supabase URL and API key
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-// const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
-// const supabase = createClient(supabaseUrl, supabaseKey);
-// Initialize the OpenAI client with your API key
+
 const openai = new OpenAI({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 });
@@ -27,7 +23,8 @@ export async function POST(request: Request) {
   });
 
   const [{ embedding }] = openAiEmbeddings.data;
-  console.log("embedding1111", embedding);
+
+  console.log("embedding11", embedding);
 
   // Search Supabase
   const { data, error } = await supabaseClient.rpc("vector_search", {
@@ -36,14 +33,12 @@ export async function POST(request: Request) {
     match_count: 5,
   });
 
-  // const { data, error } = await supabaseClient.rpc("hello_world");
-  // query ChatGPT via Langchain, pass the query and database results as context
+  console.log("dataRes11", data);
 
   if (data) {
-    console.log(data);
+    console.log("dataResponse", data);
     return NextResponse.json({ data });
   }
-  console.log(error);
 
   return NextResponse.json({ error });
 }
